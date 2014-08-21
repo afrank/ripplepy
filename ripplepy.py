ca6d5e4d8f8fa3f2d3c92e9ad9e826b89ad1107f
Mark Travis <mtravis@ripple.com>
2014-08-21 01:46:25 -0700

    use websocket-client with smarter exception usage change log to a dictionary

--- ripplepy.py
+++ ripplepy.py
@@ -7,17 +7,7 @@ import enum
 import sys
 import time
 import json
-
-class RippleLog:
-    def __init__(self, connectionString=str(), activity=str(), remoteIP=str(), connectTime=0.0, disconnectTime=0.0,
-             activityTime=0.0, exception=list()):
-        self._connectionString = connectionString
-        self._activity = activity
-        self._remoteIP = remoteIP
-        self._connectTime = connectTime
-        self._disconnectTime = disconnectTime
-        self._activityTime = activityTime
-        self._exception = list(exception)
+import ssl
 
 class Ripple:
     class ConnectionType(enum.Enum):
@@ -34,12 +24,16 @@ class Ripple:
 #    _rpc = urllib.request.urlopen
     _timeout = None
     _isConnected = False
-    _log = None
+    # log keys: connectionString, activity, remoteIP, connectTime, disconnectTime
+    # activityStartTime, activityFinishTime, exception
+    _log = dict()
 
     def __init__(self, connectionString, timeout=None, no_ssl_verify=False):
         self._connectionString = connectionString
         self._timeout = timeout
         self._no_ssl_verify = no_ssl_verify
+        self._log = dict()
+        self._log["connectionString"] = connectionString
 
         parsedUrl = urllib.parse.urlparse(self._connectionString)
 
@@ -48,31 +42,35 @@ class Ripple:
         elif parsedUrl.scheme == 'ws' or parsedUrl.scheme == 'wss':
             self._connectionType = Ripple.ConnectionType.websocket
 
-        self._log = RippleLog(self._connectionString)
-
     def connectRippled(self):
         if self._connectionType == Ripple.ConnectionType.websocket:
             if self._isConnected:
                 self.disconnectRippled()
 
-            remote_ip = dict()
-
             try:
-                self._ws = websocket.create_connection(self._connectionString, self._timeout, remote_ip=remote_ip,
-                                                       no_ssl_verify=self._no_ssl_verify)
+                if self._no_ssl_verify:
+                    self._ws = websocket.create_connection(self._connectionString, self._timeout,
+                                                           sslopt={"cert_reqs" : ssl.CERT_NONE})
+                else:
+                    self._ws = websocket.create_connection(self._connectionString, self._timeout)
+
                 self._isConnected = True
-                self._log._exception = list()
-            except:
-                self._log._exception = list(sys.exc_info())
-            finally:
-                self._log._activity = "connect"
-                self._log._remoteIP = remote_ip["remote_ip"]
-                self._log._connectTime = time.time()
+                self._log["remoteIP"] = self._ws.sock.getpeername()[0]
+                self._log["connectTime"] = time.time()
+                if "exception" in self._log:
+                    del self._log["exception"]
+            except Exception as e:
+                if hasattr(e, 'remote_ip'):
+                    self._log["remoteIP"] = e.remote_ip
+                self._log["exception"] = sys.exc_info()
+                self._log["disconnectTime"] = time.time()
+
+            self._log["activity"] = "connect"
 
         return self._isConnected
 
     def getRemoteIP(self):
-        return self._log._remoteIP
+        return self._log["remoteIP"]
 
     def getIsConnected(self):
         return self._isConnected
@@ -81,13 +79,14 @@ class Ripple:
         if self._connectionType == Ripple.ConnectionType.websocket:
             try:
                 self._ws.close()
-                self._log._exception = list()
+                if "exception" in self._log:
+                    del self._log["exception"]
             except:
-                self._log._exception = list(sys.exc_info())
+                self._log["exception"] = sys.exc_info()
 
             self._isConnected = False
-            self._log._activity = "disconnect"
-            self._log._disconnectTime = time.time()
+            self._log["activity"] = "disconnect"
+            self._log["disconnectTime"] = time.time()
 
     def command(self, command, activity=None, params=None, id=None):
         input = { "method" : str(command) }
@@ -98,18 +97,19 @@ class Ripple:
 
         if self._connectionType == Ripple.ConnectionType.rpc:
             try:
-                self._log._connectTime = time.time()
+                self._log["connectTime"] = time.time()
                 reply = urllib.request.urlopen(self._connectionString, json.dumps(input).encode(),timeout=self._timeout)
                 output = json.loads(reply.read().decode())
-                self._log._exception = list()
+                if "exception" in self._log:
+                    del self._log["exception"]
             except:
-                self._log._exception = list(sys.exc_info())
+                self._log["exception"] = sys.exc_info()
                 output = None
-            finally:
-                self._log._disconnectTime = time.time()
-                self._log._activity = activity
 
-            return output
+            self._log["disconnectTime"] = time.time()
+            self._log["activity"] = activity
+
+        return output
 
     def cmd_server_info(self):
         return self.command("server_info", activity="server_info")
