62e5381cbc190a33dc47f09b38531ee32f5a5d6e
Mark Travis <mtravis@ripple.com>
2014-09-01 16:41:23 -0700

    PEP8 edits

--- ripplepy.py
+++ ripplepy.py
@@ -11,6 +11,7 @@ import ssl
 import http.client
 import socket
 
+
 class Ripple:
     class ConnectionType(enum.Enum):
         none = 0
@@ -24,22 +25,23 @@ class Ripple:
     _parsedUrl = None
     _ws = websocket.WebSocket
     _rpc = http.client
-    _timeout = None
     _isConnected = False
-    # log keys: connectionString, activity, remoteIP, connectTime, disconnectTime
+    # log keys: connectionString, activity, remoteIP, connectTime,
+    # disconnectTime
     # activityStartTime, activityFinishTime, exception
     _log = dict()
 
-    def __init__(self, connectionString, timeout=None, no_ssl_verify=False):
-        self._connectionString = connectionString
+    def __init__(self, connection_string, timeout=None, no_ssl_verify=False):
+        self._connectionString = connection_string
         self._timeout = timeout
         self._no_ssl_verify = no_ssl_verify
         self._log = dict()
-        self._log["connectionString"] = connectionString
+        self._log["connectionString"] = connection_string
 
         self._parsedUrl = urllib.parse.urlparse(self._connectionString)
 
-        if self._parsedUrl.scheme == 'http' or self._parsedUrl.scheme == 'https':
+        if self._parsedUrl.scheme == 'http'\
+                or self._parsedUrl.scheme == 'https':
             self._connectionType = Ripple.ConnectionType.rpc
             socket.setdefaulttimeout(self._timeout)
         elif self._parsedUrl.scheme == 'ws' or self._parsedUrl.scheme == 'wss':
@@ -51,24 +53,29 @@ class Ripple:
         if self._isConnected:
             self.disconnect()
 
-        # http.client does not actually connect the socket until a request is sent
+        # http.client does not actually connect the socket until a request is
+        # sent
         if self._connectionType == Ripple.ConnectionType.rpc:
             if self._parsedUrl.scheme == "https":
                 context = ssl.create_default_context()
-                self._rpc = http.client.HTTPSConnection(self._parsedUrl.netloc, timeout=self._timeout,
-                                                            context=context)
+                self._rpc = http.client.HTTPSConnection(self._parsedUrl.netloc,
+                                                        timeout=self._timeout,
+                                                        context=context)
             elif self._parsedUrl.scheme == "http":
-                self._rpc = http.client.HTTPConnection(self._parsedUrl.netloc, timeout=self._timeout)
+                self._rpc = http.client.HTTPConnection(self._parsedUrl.netloc,
+                                                       timeout=self._timeout)
 
             self._isConnected = True
 
         elif self._connectionType == Ripple.ConnectionType.websocket:
             try:
                 if self._no_ssl_verify:
-                    self._ws = websocket.create_connection(self._connectionString, self._timeout,
-                                                           sslopt={"cert_reqs" : ssl.CERT_NONE})
+                    self._ws = websocket.create_connection(
+                        self._connectionString, self._timeout,
+                        sslopt={"cert_reqs": ssl.CERT_NONE})
                 else:
-                    self._ws = websocket.create_connection(self._connectionString, self._timeout)
+                    self._ws = websocket.create_connection(
+                        self._connectionString, self._timeout)
 
                 self._isConnected = True
                 self._log["remoteIP"] = self._ws.sock.getpeername()[0]
@@ -85,10 +92,10 @@ class Ripple:
 
         return self._isConnected
 
-    def getRemoteIP(self):
+    def get_remote_ip(self):
         return self._log["remoteIP"]
 
-    def getIsConnected(self):
+    def get_is_connected(self):
         return self._isConnected
 
     def disconnect(self):
@@ -107,17 +114,20 @@ class Ripple:
         self._log["activity"] = "disconnect"
         self._log["disconnectTime"] = time.time()
 
-    def command(self, command, activity=None, params=None, id=None):
-        input = { "method" : str(command) }
-        if id is not None:
-            input["id"] = id
+    def command(self, command, activity=None, params=None, id_arg=None):
+        indata = {"method": str(command)}
+        if id_arg is not None:
+            indata["id"] = id_arg
         if params is not None:
-            input["params"] = params
+            indata["params"] = params
 
+        output = None
         if self._connectionType == Ripple.ConnectionType.rpc:
             try:
                 self._log["connectTime"] = time.time()
-                reply = urllib.request.urlopen(self._connectionString, json.dumps(input).encode(),timeout=self._timeout)
+                reply = urllib.request.urlopen(self._connectionString,
+                                               json.dumps(indata).encode(),
+                                               timeout=self._timeout)
                 output = json.loads(reply.read().decode())
                 if "exception" in self._log:
                     del self._log["exception"]
@@ -133,9 +143,12 @@ class Ripple:
     def cmd_server_info(self):
         return self.command("server_info", activity="server_info")
 
-    def cmd_ledger(self, ledger, full=False, accounts=False, transactions=False, expand=False):
+    def cmd_ledger(self, ledger, full=False, accounts=False, transactions=False,
+                   expand=False):
         params = list()
-        params.append({"full" : full, "accounts" : accounts, "transactions" : transactions, "expand" : expand})
+        params.append({"full": full, "accounts": accounts,
+                       "transactions": transactions, "expand": expand})
         params[0]["ledger"] = ledger
 
-        return self.command("ledger", activity="ledger," + str(ledger), params=params)
+        return self.command("ledger", activity="ledger," + str(ledger),
+                            params=params)
